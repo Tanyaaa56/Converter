d54d71d549efea43056a872025edfc3651e9d687
Tanyaaa56 <98040348+Tanyaaa56@users.noreply.github.com>
2026-03-12 00:26:06 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,34 +1,33 @@
-from sqlalchemy import String, Float
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
-from sqlalchemy import create_engine
 import os
+from sqlalchemy import String, Float, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
+
 Session = sessionmaker(bind=engine)
 
-class Base(DeclarativeBase):
-    def create_db(self):
-        Base.metadata.create_all(engine)
 
-    def drop_db(self):
-        Base.metadata.drop_all(engine)
+class Base(DeclarativeBase):
+    pass
+
 
 class Conversion(Base):
-    __tablename__ = 'conversions'
+    __tablename__ = "conversions"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     from_currency: Mapped[str] = mapped_column(String(3))
     to_currency: Mapped[str] = mapped_column(String(3))
-
-    # Сума, яку користувач хоче конвертувати
     amount: Mapped[float] = mapped_column(Float)
-
     result: Mapped[float] = mapped_column(Float)
 
 
-base = Base()
-base.create_db()
+Base.metadata.create_all(engine)
+
 
 
 
